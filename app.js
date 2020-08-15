43aa0564680ec8477f5344d1f89da0ec15991cbb
Tushar <tusharmittal429@gmail.com>
2020-08-15 05:33:38 +0530

    added hashing of passwords using md5 package

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 //jshint esversion:6
-require('dotenv').config();
+// require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+var md5 = require('md5');
 
 const app = express();
 
@@ -26,7 +27,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]  });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]  });
 
 const User = mongoose.model('User', userSchema);
 
@@ -46,7 +47,7 @@ app.post("/register",function(req,res){
 
     const newUser = new User({
       email: req.body.username,
-      password: req.body.password
+      password: md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -67,7 +68,7 @@ app.post("/login",function(req,res){
       }
       else{
         if(foundItem){
-          if(foundItem.password === req.body.password){
+          if(foundItem.password === md5(req.body.password)){
             res.render("secrets");
           }
           else{
